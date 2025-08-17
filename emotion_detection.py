9b878b09e756c4821e689f24ad5b2db419cde3a2
Devi-debug <itsomm143@gmail.com>
2025-08-17 12:06:31 -0400

    Formatted the otput to json format

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,8 +1,16 @@
 import requests
+import json
 def emotion_detector(text_to_analyze):
     url='https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     headers={"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     myobj={ "raw_document": { "text": text_to_analyze } }
     response=requests.post(url,headers=headers,json=myobj)
-    return response.text
-    
+    formatted_response=json.loads(response.text)
+    emotions=formatted_response['emotionPredictions'][0]['emotion']
+    print("Overall emotion scores")
+    for emotion,score in emotions.items():
+        dominant_emotion=max(emotions, key=emotions.get)
+        return {"anger":score,"disgust":score,"fear":score,
+        "joy":score,"sadness":score,"dominant_emotion":dominant_emotion}
+
+        
